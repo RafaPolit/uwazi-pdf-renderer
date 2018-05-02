3ca9ace6de33b874b904a15d8502c9580accd26a
RafaPolit <rafaelpolit@gmail.com>
2018-05-02 17:27:12 -0500

    Failed attempts at testing PDFjs without mock.

--- components/PDF.jsx
+++ components/PDF.jsx
@@ -1,3 +1,61 @@
-export default () => {
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import pdfjsLib from 'pdfjs-dist';
 
-};
+import Page from './Page';
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../node_modules/pdfjs-dist/build/pdf.worker.js';
+
+export default class PDF extends Component {
+  static defaultProps = {
+    Loading: () => {},
+  }
+
+  static propTypes = {
+    url: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.string,
+    ]).isRequired,
+    Loading: PropTypes.func,
+  };
+
+  state = { pdfDocument: null }
+
+  componentDidMount() {
+    // console.log('prior to get:', pdfjsLib);
+  }
+
+  getDocument() {
+    return pdfjsLib.getDocument(this.props.url)
+      .then((pdfDocument) => {
+        console.log('about to set state');
+        this.setState({ pdfDocument });
+        return pdfDocument;
+      })
+      .catch((err) => {
+        console.log('ERR:', err);
+      });
+  }
+
+  render() {
+    const { Loading } = this.props;
+    const { pdfDocument } = this.state;
+
+    if (!pdfDocument) {
+      return <Loading />;
+    }
+
+    console.log('numPages:', pdfDocument.numPages);
+
+    const pages = [];
+    for (let page = 1; page <= pdfDocument.numPages; page += 1) {
+      pages.push(<Page
+        key={page}
+        page={page}
+        pdf={pdfDocument}
+      />);
+    }
+
+    return <div>{pages}</div>;
+  }
+}
--- components/specs/PDF.spec.jsx
+++ components/specs/PDF.spec.jsx
@@ -1,12 +1,69 @@
 import React from 'react';
+import path from 'path';
+import fs from 'fs';
+import pdfjsLib from 'pdfjs-dist';
 import { shallow } from 'enzyme';
 
 import PDF from '../PDF';
 import Page from '../Page';
 
+/* Check where is the better place for this? */
+pdfjsLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
+
 describe('PDF', () => {
-  it('should render one Page component for each page', () => {
-    const component = shallow(<PDF />);
-    expect(component.find(Page).length).toBe(29);
+  let component;
+  let instance;
+  let Loading;
+  let globalWindow;
+
+  const readPDFData = pdfURL => new Promise((resolve, reject) => {
+    fs.readFile(pdfURL, (err, rawData) => {
+      if (err) { reject(err); }
+      resolve(new Uint8Array(rawData));
+    });
+  });
+
+  const render = async (props) => {
+    const data = await readPDFData(path.join(__dirname, 'files/batman.pdf'));
+    const serverClientMock = {
+      data,
+      disableFontFace: true,
+      nativeImageDecoderSupport: 'none',
+    };
+
+    component = shallow(<PDF url={serverClientMock} {...props} />);
+    instance = component.instance();
+  };
+
+  beforeEach(() => {
+    globalWindow = global.window;
+    delete global.window;
+  });
+
+  afterEach(() => {
+    global.window = globalWindow;
+  });
+
+  it('should render a default Loading', async () => {
+    await render();
+    expect(component.type()).toBe(component.instance().props.Loading);
+  });
+
+  it('should render the passed component when loading', async () => {
+    Loading = () => <div className="Loading" />;
+    await render({ Loading });
+    expect(component.type()).toBe(Loading);
+  });
+
+  fit('should render one Page component for each page', (done) => {
+    render()
+      .then(() => {
+        console.log('aqui llego en Test:');
+        return instance.getDocument();
+      })
+      .then((pdfDocument) => {
+        console.log('Tengo PDF:', pdfDocument);
+        done();
+      });
   });
 });
