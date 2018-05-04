5cf219cdfec946e263d9098f0f99e0aa1ecb54c4
Daneryl <daneryl@gmail.com>
2018-05-04 18:16:52 +0200

    rangeClientRects now detects parent containers if css selector passed

--- src/helpers/rangeClientRects.js
+++ src/helpers/rangeClientRects.js
@@ -1,7 +1,15 @@
 import rangeWrapper from './rangeWrapper';
 
+const findAncestor = (element, className) => {
+  let node = element;
+  while (node && !node.classList.contains(className)) {
+    node = node.parentElement;
+  }
+  return node;
+};
+
 export default {
-  get(range, parentNode) {
+  get(range, parentCssSelector, pageAttribute) {
     const elementWrapper = document.createElement('span');
     const wrapped = rangeWrapper.wrap(range, elementWrapper);
 
@@ -12,19 +20,21 @@ export default {
       scrollLeft: 0,
     };
 
-    if (parentNode) {
-      parentNodeRect = parentNode.getBoundingClientRect();
-      parentNodeRect.scrollTop = parentNode.scrollTop;
-      parentNodeRect.scrollLeft = parentNode.scrollLeft;
-    }
-
     const rects = wrapped.nodes.map((node) => {
+      const parentNode = findAncestor(node, parentCssSelector);
+      if (parentNode) {
+        parentNodeRect = parentNode.getBoundingClientRect();
+        parentNodeRect.scrollTop = parentNode.scrollTop;
+        parentNodeRect.scrollLeft = parentNode.scrollLeft;
+        parentNodeRect.page = parentNode.getAttribute(pageAttribute);
+      }
       const rect = node.getClientRects()[0];
       return {
         top: rect.top + (parentNodeRect.scrollTop - parentNodeRect.top),
         left: rect.left + (parentNodeRect.scrollLeft - parentNodeRect.left),
         width: rect.width,
         height: rect.height,
+        page: parentNodeRect.page,
       };
     });
 
--- src/helpers/specs/rangeClientRects.spec.js
+++ src/helpers/specs/rangeClientRects.spec.js
@@ -1,27 +1,12 @@
 import rangeClientRects from '../rangeClientRects';
 import wrapper from '../rangeWrapper';
 
-const node = {
-  getClientRects() {
-    return [{
-      top: 10,
-      left: 11,
-      width: 12,
-      height: 13,
-    }];
-  },
-};
-
-const parentNode = {
-  getBoundingClientRect() {
-    return {
-      top: 5,
-      left: 5,
-    };
-  },
-  scrollTop: 1,
-  scrollLeft: 2,
-};
+const getClientRects = () => [{
+  top: 10,
+  left: 11,
+  width: 12,
+  height: 13,
+}];
 
 const relativeNodeRect = {
   top: 6,
@@ -30,16 +15,51 @@ const relativeNodeRect = {
   height: 13,
 };
 
+const mockPage = (id) => {
+  const page = document.getElementById(id);
+  page.getBoundingClientRect = () => ({
+    top: 5,
+    left: 5,
+  });
+  page.scrollTop = 1;
+  page.scrollLeft = 2;
+};
+
 describe('rangeClientRects', () => {
   let wrapped;
   beforeEach(() => {
+    document.body.innerHTML = `
+      <div>
+        <div class='page' id='page1' data-page='1'>
+          <div id='node1'></div>
+          <div>
+            <div id='node2'></div>
+          </div>
+        </div>
+        <div class='page' id='page2' data-page='2'>
+          <div id='node3'></div>
+        </div>
+      </div>
+    `;
+
+    const node1 = document.getElementById('node1');
+    node1.getClientRects = getClientRects;
+    const node2 = document.getElementById('node2');
+    node2.getClientRects = getClientRects;
+    const node3 = document.getElementById('node3');
+    node3.getClientRects = getClientRects;
+
+    mockPage('page1');
+    mockPage('page2');
+
     wrapped = {
       unwrap: () => {
         wrapped.nodes = [];
       },
-      nodes: [node, node, node],
+      nodes: [node1, node2, node3],
     };
   });
+
   it('should return all client rects based on a range selection', () => {
     const range = 'RangeObject';
     spyOn(wrapper, 'wrap').and.returnValue(wrapped);
@@ -48,22 +68,26 @@ describe('rangeClientRects', () => {
     const rects = rangeClientRects.get(range);
     expect(wrapper.wrap).toHaveBeenCalledWith(range, elementWrapper);
     expect(rects).toEqual([
-      node.getClientRects()[0],
-      node.getClientRects()[0],
-      node.getClientRects()[0],
+      getClientRects()[0],
+      getClientRects()[0],
+      getClientRects()[0],
     ]);
     expect(wrapped.nodes).toEqual([]);
   });
 
-  describe('when passing a parent node', () => {
-    it('should compute top and left relative to the parent', () => {
+  describe('when passing a parent node selector', () => {
+    it('should compute top and left relative to the parent and set the page information to the rect', () => {
       const range = 'RangeObject';
       spyOn(wrapper, 'wrap').and.returnValue(wrapped);
 
       const elementWrapper = document.createElement('span');
-      const rects = rangeClientRects.get(range, parentNode);
+      const rects = rangeClientRects.get(range, 'page', 'data-page');
       expect(wrapper.wrap).toHaveBeenCalledWith(range, elementWrapper);
-      expect(rects).toEqual([relativeNodeRect, relativeNodeRect, relativeNodeRect]);
+      expect(rects).toEqual([
+        Object.assign({}, relativeNodeRect, { page: '1' }),
+        Object.assign({}, relativeNodeRect, { page: '1' }),
+        Object.assign({}, relativeNodeRect, { page: '2' }),
+      ]);
     });
   });
 });
